1e9110950c3f77d8c54a7283ce285f69b048fb87
Amirdhesh <amirdhesh2003@gmail.com>
2025-02-20 13:24:06 +0530

    stashed

--- Wrapper.java
+++ Wrapper.java
@@ -4,6 +4,6 @@ public class Wrapper {
     public static void main(String[] args){
         int a = 5;
         Integer b = a;
-        System.out.println(b);
+        System.out.println(b+5);
     }
 }
